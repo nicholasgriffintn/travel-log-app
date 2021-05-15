dc5f46ddf525870faf563b9065d21a42646526a0
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-05-15 22:31:22 +0100

    Fix: wrong env var

--- client/src/store/actions/constants.js
+++ client/src/store/actions/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = `${
-  process.env.NODE_ENV === 'production' ? process.env.API_URL : ''
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : ''
 }`;
 
 export const GET_HEALTH = 'GET_HEALTH';
